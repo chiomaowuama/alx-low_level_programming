f57161ee2b71fbed3127b5082d822af0f03e27ce
chiomaowuama <chioma12198@gmail.com>
2023-02-22 10:20:18 -0800

    this was done by me

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include  "main.h" 
+
+/**
+ * add - This is two add two ints together.
+ * @n1: The first number.
+ * @n2: The second number.
+ * Return - The result of the addition.
+ */
+
+int add(int n1, int n2)
+{
+	int result = n1 + n2;
+
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * int _islower - check for lowercase.
+ * Return: int
+ */
+int _islower(int c)
+{
+   
+        if(c >= 97 && c <= 122)
+        {       
+        return(1);
+        }              
+         return(0);
+              
+} 
+
