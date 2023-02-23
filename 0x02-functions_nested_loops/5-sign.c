2c1117ef368478e611b54dffcc4d65522f008a09
chiomaowuama <chioma12198@gmail.com>
2023-02-22 16:08:33 -0800

    the update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+/**
+ * print_sign - check the code.
+ * @n: this is the number.
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+}
