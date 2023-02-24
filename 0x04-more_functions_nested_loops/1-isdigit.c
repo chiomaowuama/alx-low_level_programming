98cc006adb98f4d04eba51ae6d20afa52c5621c9
chiomaowuama <chioma12198@gmail.com>
2023-02-23 17:10:22 -0800

    the nested loop 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  _isdigit - check the code
+ * @x: the digit to be used .
+ * Return: Always 0.
+ */
+
+int _isdigit(int x)
+{
+	if (x >= 45 && x <= 57)
+	{
+	return(1);
+	}
+	return(0);
+}
