8733dfd414a61c74607a6540ce72566cd9ddc0ad
chiomaowuama <chioma12198@gmail.com>
2023-02-23 17:17:45 -0800

    the nested moop 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *mul- check the code
+ * @a: the first digit to be used.
+ * @b: the seconf digit to be used 
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b)
+}
