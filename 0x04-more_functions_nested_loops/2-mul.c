4b1395d87154f718199ecafc96982be6fd305f17
chiomaowuama <chioma12198@gmail.com>
2023-02-23 17:24:25 -0800

    the nested loop 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- *mul- check the code
+ * mul- check the code
  * @a: the first digit to be used.
  * @b: the seconf digit to be used 
- * Return: Always 0.
+ * Return: the m ultiplication of the a and the b.
  */
 int mul(int a, int b)
 {
-	return (a * b)
+	return (a * b);
 }
