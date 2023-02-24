7edec62697d0fae2a8e1383e24d43f808dac5fea
chiomaowuama <chioma12198@gmail.com>
2023-02-23 17:33:48 -0800

    the nested loop

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ * @c: this i the digit to be used .
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int x;
+	
+	for (x =0; x <= 9; x++)
+	{
+	_putchar(x + '0');
+	_putchar('\n');
+}
+	
+
