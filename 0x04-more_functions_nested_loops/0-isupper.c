7af05eb4016eb55e8ff34863c03d5f78194c2452
chiomaowuama <chioma12198@gmail.com>
2023-02-23 18:36:29 -0800

    the first loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,15 @@
 
 /**
  * _isupper - the function to be called.
- * @c: the variable to be used.
+ * @x: the variable to be used.
  * Return: Always 0.
  */
 
 int _isupper(int x)
 {
-	if (x >= 65 && x <+ 98)
+	if (x >= 65 && x <= 98)
 	{
-	return(1);
+	return (1);
 	}
-	return(0);
+	return (0);
 }
