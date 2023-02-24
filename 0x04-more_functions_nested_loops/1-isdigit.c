8b702bb543de8ca856fb80833c47effd0be53289
chiomaowuama <chioma12198@gmail.com>
2023-02-23 18:46:21 -0800

    the second loop

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,14 +2,14 @@
 /**
  *  _isdigit - check the code
  * @x: the digit to be used .
- * Return: Always 0.
+ * Return: this should be either 1 ot 0.
  */
 
 int _isdigit(int x)
 {
-	if (x >= 45 && x <= 57)
+	if (x >= 48 && x <= 57)
 	{
-	return(1);
+	return (1);
 	}
-	return(0);
+	return (0);
 }
