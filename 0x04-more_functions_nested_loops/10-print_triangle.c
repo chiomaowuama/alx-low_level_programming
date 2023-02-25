62ca83be58b0f313eb6fab102987c8524a84fc45
chiomaowuama <chioma12198@gmail.com>
2023-02-24 16:48:29 -0800

    the main

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * print_triangle - check the code
+ * @size: the size of the shape.
  * Return: Always 0.
  */
 void print_triangle(int size)
 {
-	int x, y, z:
-	if (size <=0)
+	int x, y, z;
+
+	if (size <= 0)
 	{
 	_putchar('\n');
 	}
