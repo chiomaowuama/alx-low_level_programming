eafe3009afe125823da5a5e69c7620fc887126c2
chiomaowuama <chioma12198@gmail.com>
2023-02-27 07:38:59 -0800

    the secod for pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * void swap_int- check the code
+ * @a: The first digit.
+ * @b: The second digit.
+ * @temp: The temporary digit.
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+
+}
+
