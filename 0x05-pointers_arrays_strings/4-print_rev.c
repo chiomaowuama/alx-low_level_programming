845252684fa92cfe612ab7ff483ef268887e531e
chiomaowuama <chioma12198@gmail.com>
2023-02-27 11:39:30 -0800

    the fifth

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+	c++;
+	}
+
+	for (c -= 1; c >= 0;c--)
+	{
+	_putchar(s[c]);
+	}
+	_putchar('\n');
+}
