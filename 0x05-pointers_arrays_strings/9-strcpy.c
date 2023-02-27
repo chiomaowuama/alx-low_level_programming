3636b21185f226193ef3d7995ac8c281f0d129de
chiomaowuama <chioma12198@gmail.com>
2023-02-27 14:51:45 -0800

    the last

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copy string
+ * @dest: destination of copied string
+ * @src: string to copy
+ *
+ * Return: char
+*/
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
