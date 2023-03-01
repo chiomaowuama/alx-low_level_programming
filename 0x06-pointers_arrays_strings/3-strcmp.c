8cee93162ba34cb0ed71af7cf90c9e808c54c75a
chiomaowuama <chioma12198@gmail.com>
2023-03-01 10:48:36 -0800

    the fourth

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - Function to be used.
+ * @s1: The character.
+ * @s2: The character.
+ * Return: this return th pointer.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
