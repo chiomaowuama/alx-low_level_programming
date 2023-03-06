4d8f9df81fdb81584b0a2933844774e3f4301f73
chiomaowuama <chioma12198@gmail.com>
2023-03-06 11:49:50 -0800

    the original

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,23 +1,14 @@
-#include <stdio.h>
 #include "main.h"
+#include <string.h>
 
 /**
- * _strpbrk - check the code
- * @s: the digit
- * @accept: input
- * Return: Always 0(Success).
- */
+ * _strpbrk - locate multiple characters in string
+ * @s: the string
+ * @accept: the charset
+ *
+ * Return: char
+*/
 char *_strpbrk(char *s, char *accept)
 {
-	int i, n;
-
-	for (i = 0; s[i] != '\0'; n++)
-	{
-		for (n = 0; accept[n] != '\0'; n++)
-		{
-			if (s[i] == accept[n])
-				return (s + i);
-		}
-	}
-	return (NULL);
+	return (strpbrk(s, accept));
 }
