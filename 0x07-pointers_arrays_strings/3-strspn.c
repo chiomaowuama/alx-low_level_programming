4a5aa9d4c1d20ae4a88689d7a5383e368d6f28c9
chiomaowuama <chioma12198@gmail.com>
2023-03-06 11:47:51 -0800

    the redo

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,31 +1,14 @@
 #include "main.h"
-#include <stdio.h>
+#include <string.h>
 
 /**
- * int_strspn - check the code
- * @n: digits to be used .
- * @i: digits to be used 
- * @value:to be used 
- * @check:value to be used 
- * Return: Always 0.
- */
+ * _strspn - span a string
+ * @s: the string
+ * @accept: the charset
+ *
+ * Return: unsigned int
+*/
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n, value, check;
-
-	value = 0;
-	for (i = 0; s[i] >= '\0'; i++)
-	{
-		check =0;
-
-		for (n = 0; accept[n] != '\0'; n++)
-		{
-			if (accept[n] == s[i])
-			{
-				value++;
-				check = 1;
-			}
-		}
-	}
-	
+	return (strspn(s, accept));
 }
