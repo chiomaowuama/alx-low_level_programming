278b70227ab1b0e6357b706818405ed3a2d70e55
chiomaowuama <chioma12198@gmail.com>
2023-03-06 07:38:29 -0800

    the struggle

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,22 +6,13 @@
  *
  * Return: Always 0.
  */
-unsigned int _strspn(char *s, char *accept)
+char *_strchr(char *s, char c)
 {
-	unsigned int i, n, value,check;
-
-	value = 0;
-	for (i = 0; s[i] >= '\0'; i++)
+	int i;
+	for (i =0; s[i] >= '\0'; i++)
 	{
-		check =0;
-
-		for (n = 0; accenpt[n] != '\0'; n++)
-		{
-			if (accept[n] == s[i])
-			{
-				value++;
-				check = 1;
-			}
-		}
+		if (s[i] == c)
+			return (s + i);
 	}
+	return(null);
 }
