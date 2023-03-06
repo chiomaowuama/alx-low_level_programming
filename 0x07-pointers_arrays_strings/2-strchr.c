0079430f2c0988b023f68c9f32c357def91d0453
chiomaowuama <chioma12198@gmail.com>
2023-03-06 07:13:34 -0800

    the third

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,11 +8,20 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i;
+	unsigned int i, n, value,check;
+
+	value = 0;
 	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (s[i] == c)
-			return(s + i);
+		check =0;
+
+		for (n = 0; accenpt[n] != '\0'; n++)
+		{
+			if (accept[n] == s[i])
+			{
+				value++;
+				check = 1;
+			}
+		}
 	}
-	return (null);
 }
