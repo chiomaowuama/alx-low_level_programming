314d5a70425fbd4da3808b0e1f19d8d175a6da07
chiomaowuama <chioma12198@gmail.com>
2023-03-10 16:16:38 -0800

    the third

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	printf("%d\n", (a * b));
+
+	return (0);
+}
