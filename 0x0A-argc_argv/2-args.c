c3fb989f2c439b5f8f7bfb3353a372c99f4f8347
chiomaowuama <chioma12198@gmail.com>
2023-03-10 16:14:06 -0800

    the second

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+
+	return (0);
+}
